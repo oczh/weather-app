498d08751cdf132b8b8dcecbb8f61d4156d077a8
Czeizing Hedvig <czeizinghedi@gmail.com>
2024-01-17 14:05:34 +0100

    get the browsers language and position to the console

--- src/app/current/current.component.ts
+++ src/app/current/current.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-current',
   templateUrl: './current.component.html',
-  styleUrls: ['./current.component.scss']
+  styleUrls: ['./current.component.scss'],
 })
-export class CurrentComponent {
 
+export class CurrentComponent implements OnInit {
+  lang: string;
+  lati: number;
+  long: number;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.lang = navigator.language;
+    console.log(`browsers language: ${this.lang}`);
+    navigator.geolocation.getCurrentPosition((data) => {
+      this.lati = data.coords.latitude;
+      this.long = data.coords.longitude;
+      console.log(`browsers position:
+      geolocation coordinates latitude: ${this.lati}, 
+      geolocation coordinates longitude: ${this.long}`)
+    });
+  }
 }
