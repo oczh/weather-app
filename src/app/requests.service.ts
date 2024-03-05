45b61e96fc0297b0d5c35d623f43a1d8c192c097
oczh <138883335+oczh@users.noreply.github.com>
2024-03-05 11:49:45 +0100

    Feature/8# show the weather forecast (#16)
    * create make ForcastUrl and getForcastRequest methods and the forecastObject interface
    
    * create and call getForcastWeather method, that handle the AJAX requests
    
    * create a makeForecastWeatherObject method, that compile an object that contains the appropriate data from the response
    
    * show the table without formatting
    
    * forecast table formatting
    
    * forecast time showing with toLocaleTimeString
    
    * datas round, change row coloring setting, temp datas drawing with ngClass
    
    * types import in a new file
    
    * sunrise and sunset  setting in currentWobject with tolocaltimestring

--- src/app/requests.service.ts
+++ src/app/requests.service.ts
@@ -1,80 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
-type weatherDataObj = {
-  location: string;
-  weather_icon: string;
-  weather: string;
-  temperature : number;
-  windSpeed: number;
-  cloudness: number;
-  pressure: number;
-  humidity: number;
-  sunrise: string;
-  sunset: string;
-  rain?: number;
-  snow?: number;
-}
-
-interface currentWeatherObject {
-  coord: Coord;
-  weather: Weather[];
-  base: string;
-  main: Main;
-  visibility: number;
-  wind: Wind;
-  clouds: Clouds;
-  dt: number;
-  sys: Sys;
-  timezone: number;
-  id: number;
-  name: string;
-  cod: number;
-  rain?: Rain;
-  snow?: Snow;
-}
-interface Sys {
-  type: number;
-  id: number;
-  country: string;
-  sunrise: number;
-  sunset: number;
-}
-interface Clouds {
-  all: number;
-}
-interface Wind {
-  speed: number;
-  deg: number;
-}
-interface Main {
-  temp: number;
-  feels_like: number;
-  temp_min: number;
-  temp_max: number;
-  pressure: number;
-  humidity: number;
-}
-interface Weather {
-  id: number;
-  main: string;
-  description: string;
-  icon: string;
-}
-interface Coord {
-  lon: number;
-  lat: number;
-}
-
-interface Rain {
-  '1h' : number;
-  '3h' : number;
-}
-
-interface Snow {
-  '1h' : number;
-  '3h' : number;
-}
+import {weatherDataObj, forecastObj, currentWeatherObject, Coord, forecastWeatherObject} from './types_interfaces'
 
 @Injectable({
   providedIn: 'root'
@@ -87,6 +13,8 @@ export class RequestsService {
   units: string; 
   currentWeather: currentWeatherObject;
   currentWeatherData = {} as weatherDataObj;
+  forcastWeather: forecastWeatherObject;
+  forcastWearherData = {} as forecastObj;
 
   constructor(private http: HttpClient) { 
     this.appid = '6c46aa43db7539a6daf11a763626b3d2';
@@ -110,34 +38,82 @@ export class RequestsService {
     })
   }
 
+  getForcastWeather() {
+    navigator.geolocation.getCurrentPosition((data) => {
+      this.coord.lat = data.coords.latitude;
+      this.coord.lon = data.coords.longitude;
+      this.getForcastRequest().subscribe({
+        next: (v) =>{ 
+          console.log(v)
+          this.forcastWeather = v;
+          this.makeForecastWeatherObject(v);
+        },
+        error: (e) => console.error(e),
+        complete: () => console.info('complete')
+      });
+    })
+  }
+
   makeCurrentURL(language: string, coordinates: Coord, api: string, unit: string){
-  return `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&lang=${language}&appid=${api}&units=${unit}`
+    return `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&lang=${language}&appid=${api}&units=${unit}`
+  }
+
+  makeForcastURL(language: string, coordinates: Coord, api: string, unit: string){
+    return `https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&lang=${language}&appid=${api}&units=${unit}`
   }
 
   getCurrentRequest() {
     return this.http.get<currentWeatherObject>(this.makeCurrentURL(this.lang, this.coord, this.appid, this.units));
   }   
 
+  getForcastRequest() {
+    return this.http.get<forecastWeatherObject>(this.makeForcastURL(this.lang, this.coord, this.appid, this.units));
+  }
+
   makeCurrentWeatherObject(request : currentWeatherObject){
     this.currentWeatherData.location = request.name; 
     this.currentWeatherData.weather_icon = request.weather[0].icon;
     this.currentWeatherData.weather = request.weather[0].main;
-    this.currentWeatherData.temperature = request.main.temp;
-    this.currentWeatherData.windSpeed = request.wind.speed;
+    this.currentWeatherData.temperature = Math.round(request.main.temp);
+    this.currentWeatherData.windSpeed = Math.round(request.wind.speed);
     this.currentWeatherData.cloudness = request.clouds.all;
     this.currentWeatherData.pressure = request.main.pressure;
     this.currentWeatherData.humidity = request.main.humidity;
-    this.currentWeatherData.sunrise = this.formatedDate(request.sys.sunrise);
-    this.currentWeatherData.sunset = this.formatedDate(request.sys.sunset);
+    this.currentWeatherData.sunrise = new Date((request.sys.sunrise) * 1000 ).toLocaleTimeString(this.lang, {
+      hour: 'numeric',
+      minute:'numeric'
+    });
+    this.currentWeatherData.sunset = new Date((request.sys.sunset) * 1000 ).toLocaleTimeString(this.lang, {
+      hour: 'numeric',
+      minute:'numeric'
+    });
     this.currentWeatherData.rain = request.rain?.['1h'];
     this.currentWeatherData.snow = request.snow?.['1h'];    
   }
 
-  formatedDate(unix_timestamp : number){
-    const date = new Date(unix_timestamp * 1000);
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    return hours + ':' + minutes;
+  makeForecastWeatherObject(request : forecastWeatherObject){
+    this.forcastWearherData.location = request.city.name;
+    this.forcastWearherData.forecastArr = []
+    for(let i = 0; i < request.list.length; i++){
+      let datas = {} as weatherDataObj
+      datas.time = new Date(request.list[i].dt_txt).toLocaleTimeString(this.lang, {
+        weekday: 'long',
+        // month: 'short',
+        // day: 'numeric',
+        hour: "numeric",
+        dayPeriod:"short"
+      })+'h';
+      datas.weather_icon = request.list[i].weather[0].icon;
+      datas.weather = request.list[i].weather[0].main;
+      datas.temperature = Math.round(request.list[i].main.temp);
+      datas.windSpeed = Math.round(request.list[i].wind.speed);
+      datas.cloudness = request.list[i].clouds.all;
+      datas.pressure = request.list[i].main.pressure;
+      datas.humidity = request.list[i].main.humidity;
+      datas.rain = request.list[i].rain?.['3h'];
+      datas.snow = request.list[i].snow?.['3h'];
+      this.forcastWearherData.forecastArr.push(datas);
+    }
+    console.log(this.forcastWearherData)
   }
 }
-
