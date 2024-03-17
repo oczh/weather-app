61782c596f8ee66f84b2a8fa09cfcb3a3f486a78
Czeizing Hedvig <czeizinghedi@gmail.com>
2024-03-17 19:44:40 +0100

    display form

--- src/app/location-form/location-form.component.ts
+++ src/app/location-form/location-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RequestsService } from '../requests.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-location-form',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./location-form.component.scss']
 })
 export class LocationFormComponent {
+  constructor(public requestServise: RequestsService) {
+  }
+  
+  ngOnInit(): void {
+    this.requestServise.getForcastWeather();
+  }
 
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    console.log(form.valid); 
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LocationFormComponent } from '../location-form/location-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     LocationFormComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [
     LocationFormComponent
